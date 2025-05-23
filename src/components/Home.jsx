eac88fb021af2cc78c37ebba3f8b9e4980fa4269
ujjwalukstar India <ujjwalkumar94310@gmail.com>
2025-05-24 00:00:06 +0530

    Home.jsx added

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>This is Home Page</h1>
+      </div>
+
+      <h2>Blink Tac Toe</h2>
+    </>
+  );
+};
+
+export default Home;
